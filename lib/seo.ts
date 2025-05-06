f132f250ae64bf39bc40a6cc5a7344f81a427c2c
aungsannphyo <mgkalay1995@gmail.com>
2025-05-06 23:34:45 +0630

    Update seo.ts

--- lib/seo.ts
+++ lib/seo.ts
@@ -3,7 +3,7 @@
 export const defaultMeta = {
     title: "Aung Sann Phyo – Software Engineer",
     description: "Personal portfolio of Aung Sann Phyo, a software engineer skilled in frontend, backend and mobile technologies.",
-    url: "https://portfolio-red-seven-64.vercel.app/my-portfolio",
+    url: "https://portfolio-red-seven-64.vercel.app/profile.jpeg",
     image: "https://portfolio-red-seven-64.vercel.app/my-portfolio/profile.jpeg",
     type: "website",
 };
