1c3f6c2987cdc800cea1988cab9bcb13ef7fe42f
aungsanphyo <aungsanphyo@immin.io>
2025-05-09 00:39:15 +0630

    change content to article series

---
+++ app/article-series/[slug]/page.tsx
@@ -0,0 +1,57 @@
+import BackButton from "@/components/back-buttom";
+import MarkdownRenderer from "@/features/articles/markdown-renderer";
+import { getMarkdownArticleData } from "@/app/api/markdown";
+
+export default async function ArticleDetails({
+  params,
+  searchParams,
+}: {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ key?: string }>;
+}) {
+  const { slug } = await params;
+  const search = await searchParams;
+  const key = search.key || "default";
+
+  let markdown: string | null = null;
+  let article = null;
+  let errorMessage: string | null = null;
+
+  try {
+    const data = await getMarkdownArticleData(slug, key);
+    markdown = data.markdown;
+    article = data.article ?? null;
+  } catch (err) {
+    if (err instanceof Error) {
+      errorMessage = err.message;
+    } else {
+      errorMessage = "An unknown error occurred.";
+    }
+  }
+
+  if (!article || !markdown) {
+    return (
+      <div className="text-center text-red-600 font-semibold mt-8">
+        ⚠️ Oops! We couldn&apos;t load the article content.
+        <div className="text-sm text-gray-500 mt-2">
+          {errorMessage?.length !== 0 &&
+            "Please try again later or contact support if the issue persists."}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex justify-center px-4">
+      <div
+        className="prose prose-sm sm:prose-base md:prose-md prose-slate dark:prose-invert 
+                    prose-headings:font-semibold prose-code:before:content-none 
+                    prose-code:after:content-none prose-pre:not-prose w-full max-w-3xl tracking-wide"
+      >
+        <BackButton />
+        <MarkdownRenderer content={markdown} />
+        <BackButton />
+      </div>
+    </div>
+  );
+}
---
+++ app/article-series/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import ContentCategories from "@/features/content/content-categories";
+import React from "react";
+
+const ContentPage = () => {
+  return (
+    <main className="max-w-5xl mx-auto px-4 md:px-0 space-y-6">
+      <div className="bg-yellow-100 text-yellow-800 px-4 py-4 rounded text-center dark:bg-yellow-900 dark:text-yellow-200">
+        📢 <strong>Announcement:</strong> To ensure you can read the contents
+        properly, it is necessary to keep the VPN turned on.
+      </div>
+      <ContentCategories />
+    </main>
+  );
+};
+
+export default ContentPage;
